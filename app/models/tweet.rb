24fbbaab5bc9701dd93e6e3d309a33092858c87c
mark0802-prog <hayabua.deu.horn@live.jp>
2020-10-15 00:23:51 +0900

    アソシエーションの宣言

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
+  belongs_to :user
+
   validates :message, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :tweets, dependent: :destroy
+
   validates :user_name, presence: true
 end
