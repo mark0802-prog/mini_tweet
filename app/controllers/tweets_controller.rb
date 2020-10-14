e1be8c4e79676a9c968bce2f97a6c5d6a58c33d6
mark0802-prog <hayabua.deu.horn@live.jp>
2020-10-15 00:24:16 +0900

    ツイート投稿・表示のコントローラ・ビューを実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,21 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.includes(:user).reverse_order
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message).merge(user_id: current_user.id)
   end
 end
