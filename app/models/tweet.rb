aa06d2e52d5e5e965ddf4c8a1bdc34506e4858c1
mark0802-prog <hayabua.deu.horn@live.jp>
2020-10-30 22:49:52 +0900

    アソシエーションの追加

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class Tweet < ApplicationRecord
+  has_many :favorites
+  has_many :users, through: :favorites
   belongs_to :user
 
   validates :message, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :user_name, presence: true
 end
